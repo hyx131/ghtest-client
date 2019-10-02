9f4aa59c70b40c088f6f8157f48557c86f2d43d1
Selin Huang <selin.hyx@gmail.com>
2019-10-02 08:43:22 -0400

    installed and used "react-testing-library"

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import App from "../App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// import ReactDOM from 'react-dom';
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+/**************************** TESTING WITH REACT-TESTING-LIBRARY *****************************/
+
+// 'react-testing-library' has better syntax
+// JEST renders the components onto jsdom
+//     --> executes html/css/js files the same way a real browser does (just doesn't paint anything to the UI)
+// rendering components = time-consuming, thus need to unmount/cleanup component after each test
+
+import { render, cleanup } from "@testing-library/react";
+
+afterEach(cleanup);
+
+it("renders without creashing", () => {
+  render(<App />);
 });
