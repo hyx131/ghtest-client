d3e25ec3bb545519d198c40a2c426101dd8bdc34
Selin Huang <selin.hyx@gmail.com>
2019-10-02 08:58:41 -0400

    added gloabals setup file for testing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -10,15 +10,15 @@ import App from "../App";
 
 /**************************** TESTING WITH REACT-TESTING-LIBRARY *****************************/
 
-// 'react-testing-library' has better syntax
-// JEST renders the components onto jsdom
-//     --> executes html/css/js files the same way a real browser does (just doesn't paint anything to the UI)
-// rendering components = time-consuming, thus need to unmount/cleanup component after each test
-
+// use 'react-testing-library' --> has better syntax
 import { render, cleanup } from "@testing-library/react";
 
+// JEST renders the components onto JSDOM
+//     --> executes html/css/js files the same way a real browser does (just doesn't paint anything to the UI)
+// rendering components = time-consuming, thus need to unmount/cleanup component after each test
 afterEach(cleanup);
 
+// this test verifies can render the component to the DOM without crashing
 it("renders without creashing", () => {
   render(<App />);
 });
---
+++ src/setupTests.js
@@ -0,0 +1,5 @@
+// src/setupTests.js = global setup file
+
+// this import configures JEST to use JEST-DOM
+//    --> JEST-DOM library provides many matchers to be used in tests
+import "@testing-library/jest-dom/extend-expect";
