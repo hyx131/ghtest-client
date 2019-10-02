ce7b7c8fad7a257a1e2979eb3b60be77eb60160d
Selin Huang <selin.hyx@gmail.com>
2019-10-02 09:33:46 -0400

    added second test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -13,12 +13,24 @@ import App from "../App";
 // use 'react-testing-library' --> has better syntax
 import { render, cleanup } from "@testing-library/react";
 
-// JEST renders the components onto JSDOM
-//     --> executes html/css/js files the same way a real browser does (just doesn't paint anything to the UI)
-// rendering components = time-consuming, thus need to unmount/cleanup component after each test
-afterEach(cleanup);
+describe("Application", () => {
+  // JEST renders the components onto JSDOM
+  //     --> executes html/css/js files the same way a real browser does (just doesn't paint anything to the UI)
+  // rendering components = time-consuming, thus need to unmount/cleanup component after each test
+  afterEach(cleanup);
 
-// this test verifies can render the component to the DOM without crashing
-it("renders without creashing", () => {
-  render(<App />);
+  // this test verifies can render the component to the DOM without crashing
+  it("renders without crashing", () => {
+    render(<App />);
+  });
+
+  // this test uses react-testing-library && jest-dom for testing <App/> component
+  //    --> "render" = imported by react-testing-library
+  //    --> "expect" = available in global scope provided by JEST
+  //    --> "getByText" query = return by "render" (but queries come from the dom-testing-library)
+  //    --> "toBeInTheDocument" function = matcher provided by JEST-DOM (not provided by default --> configured JEST to use JEST-DOM in global setup file)
+  it("renders the message on page load", () => {
+    const { getByText } = render(<App />);
+    expect(getByText("TESTING TESTING")).toBeInTheDocument();
+  });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,5 @@
 // src/setupTests.js = global setup file
 
 // this import configures JEST to use JEST-DOM
-//    --> JEST-DOM library provides many matchers to be used in tests
+//    --> JEST-DOM library provides DOM-specific matchers to be used in tests
 import "@testing-library/jest-dom/extend-expect";
